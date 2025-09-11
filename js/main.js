70e68f735d29b03645dcb973175e71094a495a44
Santiago Curotto <santiago.curotto.98@gmail.com>
2025-09-11 12:11:50 -0300

    avances osbre estetica

--- js/main.js
+++ js/main.js
@@ -13,18 +13,19 @@ document.addEventListener("DOMContentLoaded", function () {
         function (position) {
             let latitud = position.coords.latitude
             let longitud = position.coords.longitude
-
+            
             fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitud}&longitude=${longitud}&localityLanguage=es`)
                 .then(respuesta => respuesta.json())
                 .then(data => {
-                    
+
                     const provinciaElement = document.createElement("div")
                     provinciaElement.classList.add(`prov`)
                     provinciaElement.innerHTML = `<h1>${data.principalSubdivision}</h1>`
                     contenedorUno.appendChild(provinciaElement)
+                    
                 });
 
-            fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitud}&longitude=${longitud}&hourly=temperature_2m,visibility,cloud_cover,precipitation_probability,wind_speed_10m,apparent_temperature,relative_humidity_2m&timezone=auto`)
+            fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitud}&longitude=${longitud}&hourly=temperature_2m,weather_code,cloud_cover,visibility,precipitation_probability,wind_speed_10m,apparent_temperature,relative_humidity_2m,precipitation`)
                 .then(respuesta => respuesta.json())
                 .then(data => {
                     const climas = data.hourly
@@ -39,17 +40,19 @@ document.addEventListener("DOMContentLoaded", function () {
                         const horaApi = new Date(climas.time[i])
 
                         if (horaApi.getHours() >= date.getHours() && horaApi.getDay() === date.getDay()) {//solo me esta mostrando la temperatura del dia se se ejecuta el comando
-                            
+
                             //ACA CREAMOS EL DIV QUE CONTIENE LOS DATOS DEL CLIMA
                             const climaElement = document.createElement("div")
-
-                            console.log(climas.cloud_cover[i]);
+                            climaElement.classList.add("tempImg")
 
                             climaElement.innerHTML = `
-                            <h2><strong> ${horaApi.getHours()}:00hs </strong></h2>
-                            <p><strong>Temperatura:</strong> ${climas.temperature_2m[i]}</p>
-                            <p><strong>% Nubes:</strong> ${climas.cloud_cover[i]}</p>
-                            <p><strong>% Precipitacion:</strong> ${climas.precipitation_probability[i]}</p>
+                            <div>
+                            <i class="wi wi-wmo4680-${climas.weather_code[i]}"></i>
+                            <h2><strong></strong> ${climas.temperature_2m[i]}<span> °C</span></h2>
+                            </div>
+                            <p><strong> ${horaApi.getHours()}:00hs </strong></p>
+                            <p><strong> Nubes:</strong> ${climas.cloud_cover[i]}%</p>
+                            <p><strong> Precipitacion:</strong> ${climas.precipitation_probability[i]}%</p>
                             `
 
                             //ACA LO AGREGAMOS AL CONTENEDOR "climaContenedor"
